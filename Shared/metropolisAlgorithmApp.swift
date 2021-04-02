1e96da1010e2c46cf3a0fa7121314595b4c5b0a2
Matt <madas@hawk.iit.edu>
2021-04-02 12:43:53 -0500

    Initial Commit

---
+++ Shared/metropolisAlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  metropolisAlgorithmApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct metropolisAlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
